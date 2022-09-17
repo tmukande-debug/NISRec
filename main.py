7e8404a00b5cdb71281f37b8b057e15500178ce4
zhu kun <58217249+zhukun-tju@users.noreply.github.com>
2022-09-17 15:15:21 +0800

    Update main.py

--- main.py
+++ main.py
@@ -18,8 +18,8 @@ def str2bool(s):
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--dataset', default = 'ml-1m')
-parser.add_argument('--train_dir', default = 'default')
+parser.add_argument('--dataset', required=True)
+parser.add_argument('--train_dir', required=True)
 # parser.add_argument('--batch_size', default=128, type=int)
 parser.add_argument('--batch_size', default=32, type=int)
 parser.add_argument('--short_length', default=10, type=int)
@@ -48,42 +48,42 @@ f.close()
 # SAVE_MODEL_PATH = SAVE_MODEL_DIR + f"/checkpoint.{args.dataset}.pth.tar"
 
 dataset = data_partition(args.dataset, args.maxlen)
-[user_train, user_train_valid, user_train_test, user_valid, user_test, usernum, itemnum, all_User, all_Item, User, Item,
- train_User, train_Item, n_hots, user_map, item_count] = dataset
+[user_train, user_train_valid, user_train_test, user_valid, user_test, usernum, itemnum, all_User, all_Item, User, Item, train_User, train_Item, n_hots, user_map, item_count] = dataset
+# item_count用于后面的筛选，交互次数太多的item连接的用户不需要考虑为embedding-learning的邻居，但是在最后的cos计算的时候可以考虑
 
 ###############
-# A special node2vec algorithm for Learning user representations,
-#  we use the learned representations to filter better neighbor users for the target user
-MAX_USER_SIZE = usernum
-EMBEDDING_SIZE = 100
-batch_size = 32
-dataset = NodeEmbeddingDataset(user_train, train_Item, usernum, item_count)
-dataloader = tud.DataLoader(dataset, batch_size, shuffle=True)
+#进行一个user 的node2vec的学习
+# MAX_USER_SIZE = usernum
+# EMBEDDING_SIZE = 100
+# batch_size = 32
+# dataset = NodeEmbeddingDataset(user_train, train_Item, usernum, item_count)
+# dataloader = tud.DataLoader(dataset, batch_size, shuffle=True)
 
 
-pre_model = EmbeddingModel(usernum, EMBEDDING_SIZE)
-optimizer = torch.optim.Adam(pre_model.parameters(), lr=1e-3)
+# pre_model = EmbeddingModel(usernum+1, EMBEDDING_SIZE, args.device)
+# optimizer = torch.optim.Adam(pre_model.parameters(), lr=1e-3)
+# pre_model.to(args.device)
+# pre_model.train()
+# for e in range(100):
+#     for i, (input_labels, pos_labels, neg_labels) in enumerate(dataloader):
+#         input_labels = np.array(input_labels)
+#         pos_labels = np.array(pos_labels)
+#         neg_labels = np.array(neg_labels)
+#         input_labels = input_labels
+#         pos_labels = pos_labels
+#         neg_labels = neg_labels
 
-for e in range(100):
-    for i, (input_labels, pos_labels, neg_labels) in enumerate(dataloader):
-        input_labels = np.array(input_labels)
-        pos_labels = np.array(pos_labels)
-        neg_labels = np.array(neg_labels)
-        input_labels = input_labels
-        pos_labels = pos_labels
-        neg_labels = neg_labels
+#         optimizer.zero_grad()
+#         loss = pre_model(input_labels, pos_labels, neg_labels).mean()
+#         loss.backward()
 
-        optimizer.zero_grad()
-        loss = pre_model(input_labels, pos_labels, neg_labels).mean()
-        loss.backward()
+#         optimizer.step()
 
-        optimizer.step()
+#         if i % 10 == 0:
+#             print('epoch', e, 'iteration', i, loss.item())
 
-        if i % 10 == 0:
-            print('epoch', e, 'iteration', i, loss.item())
-
-################
-
+#############
+# pre_model.eval()
 
 
 num_batch = len(user_train) // args.batch_size  # tail? + ((len(user_train) % args.batch_size) != 0)
@@ -100,7 +100,8 @@ try:
     train_neighbor_matrix = pickle.load(
         open('data/ml-1m/train_neighbor_matrix_%s_%d_%d.pickle' % (args.dataset, 10, 50), 'rb'))
 except:
-    train_neighbor_matrix = Neighbor_Op(user_train, usernum, args.maxlen, train_Item, n_hots, "train", pre_model.in_embed)
+    # Neighbor_Op之前进行一个node2vec的预处理就ok了,n_hots换成embeddings就ok了，然后直接选user就ok了
+    train_neighbor_matrix = Neighbor_Op(user_train, usernum, args.maxlen, train_Item, n_hots, "train", pre_model, args.device)
     pickle.dump(train_neighbor_matrix,
                 open('data/ml-1m/train_neighbor_matrix_%s_%d_%d.pickle' % (args.dataset, 10, 50), 'wb'))
 
@@ -109,7 +110,7 @@ try:
         open('data/ml-1m/train_neighborAt_matrix_%s_%d_%d.pickle' % (args.dataset, 10, 10), 'rb'))
 except:
     train_neighborAt_matrix = NeighborAt_Op(user_train, usernum, args.maxlen, train_Item, User, n_hots, "train",
-                                            args.short_length)
+                                            args.short_length, pre_model, args.device)
     pickle.dump(train_neighborAt_matrix,
                 open('data/ml-1m/train_neighborAt_matrix_%s_%d_%d.pickle' % (args.dataset, 10, 10), 'wb'))
 
@@ -117,7 +118,7 @@ try:
     valid_neighbor_matrix = pickle.load(
         open('data/ml-1m/valid_neighbor_matrix_%s_%d_%d.pickle' % (args.dataset, 10, 50), 'rb'))
 except:
-    valid_neighbor_matrix = Neighbor_Op(user_train_valid, usernum, args.maxlen, Item, n_hots, "valid")
+    valid_neighbor_matrix = Neighbor_Op(user_train_valid, usernum, args.maxlen, Item, n_hots, "valid", pre_model, args.device)
     pickle.dump(valid_neighbor_matrix,
                 open('data/ml-1m/valid_neighbor_matrix_%s_%d_%d.pickle' % (args.dataset, 10, 50), 'wb'))
 
@@ -126,7 +127,7 @@ try:
         open('data/ml-1m/valid_neighborAt_matrix_%s_%d_%d.pickle' % (args.dataset, 10, 10), 'rb'))
 except:
     valid_neighborAt_matrix = NeighborAt_Op(user_train_valid, usernum, args.maxlen, Item, User, n_hots, "valid",
-                                           args.short_length)
+                                           args.short_length, pre_model, args.device)
     pickle.dump(valid_neighborAt_matrix,
                 open('data/ml-1m/valid_neighborAt_matrix_%s_%d_%d.pickle' % (args.dataset, 10, 10), 'wb'))
 
@@ -134,7 +135,7 @@ try:
     test_neighbor_matrix = pickle.load(
         open('data/ml-1m/test_neighbor_matrix_%s_%d_%d.pickle' % (args.dataset, 10, 50), 'rb'))
 except:
-    test_neighbor_matrix = Neighbor_Op(user_train_test, usernum, args.maxlen, Item, n_hots, "test")
+    test_neighbor_matrix = Neighbor_Op(user_train_test, usernum, args.maxlen, Item, n_hots, "test", pre_model, args.device)
     pickle.dump(test_neighbor_matrix,
                 open('data/ml-1m/test_neighbor_matrix_%s_%d_%d.pickle' % (args.dataset, 10, 50), 'wb'))
 
@@ -143,11 +144,11 @@ try:
         open('data/ml-1m/test_neighborAt_matrix_%s_%d_%d.pickle' % (args.dataset, 10, 10), 'rb'))
 except:
     test_neighborAt_matrix = NeighborAt_Op(user_train_test, usernum, args.maxlen, Item, User, n_hots, "test",
-                                           args.short_length)
+                                           args.short_length, pre_model, args.device)
     pickle.dump(test_neighborAt_matrix,
                 open('data/ml-1m/test_neighborAt_matrix_%s_%d_%d.pickle' % (args.dataset, 10, 10), 'wb'))
 
-
+# .................................................
 sampler = WarpSampler(user_train, train_neighbor_matrix, train_neighborAt_matrix, usernum, itemnum,
                       batch_size=args.batch_size, maxlen=args.maxlen,
                       n_workers=3)
@@ -189,13 +190,13 @@ t0 = time.time()
 
 
 
-model.load_state_dict(
-    torch.load('saved_models/Toys_and_Games/checkpoint.256_20_100_2_0.1_time_2_20_32_32_0.003.pth.tar')[
-        'model_state_dict'], strict=True)
+# model.load_state_dict(
+#     torch.load('saved_models/Toys_and_Games/checkpoint.256_20_100_2_0.1_time_2_20_32_32_0.003.pth.tar')[
+#         'model_state_dict'], strict=True)
 
-model.eval()
-t_test = evaluate_valid(model, dataset, test_neighbor_matrix, test_neighborAt_matrix, args)
-print('test (NDCG@10: %.4f, HR@10: %.4f)' % (t_test[0], t_test[1]))
+# model.eval()
+# t_test = evaluate_valid(model, dataset, test_neighbor_matrix, test_neighborAt_matrix, args)
+# print('test (NDCG@10: %.4f, HR@10: %.4f)' % (t_test[0], t_test[1]))
 
 
 for epoch in range(epoch_start_idx, args.num_epochs + 1):
